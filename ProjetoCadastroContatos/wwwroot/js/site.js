929f877c487c1c9c71fcc90fbcde9deae67d5b10
Luiz Felipe Carneiro <luizsil1920@gmail.com>
2023-06-02 20:12:16 -0300

    recuperando dados

--- ProjetoCadastroContatos/wwwroot/js/site.js
+++ ProjetoCadastroContatos/wwwroot/js/site.js
@@ -1,9 +1,8 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-<<<<<<< Updated upstream
 // Write your JavaScript code.
-=======
+
 //// Write your JavaScript code.
 
 //let table = new DataTable('#table-contatos'); 
@@ -17,4 +16,3 @@ $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
 
->>>>>>> Stashed changes
